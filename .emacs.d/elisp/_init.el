914f10b619154c70c174df9b332ade9e5a2fe376
Florian Mounier <florian.mounier@kozea.fr>
2011-10-26 11:27:11 +0200

    In temp flymake

--- .emacs.d/elisp/_init.el
+++ .emacs.d/elisp/_init.el
@@ -66,9 +66,35 @@
 (when (load-file "~/.emacs.d/elisp/emacs-for-python/extensions/flymake-patch.el")
   (setq flymake-info-line-regex
         (append flymake-info-line-regex '("unused$" "^redefinition" "used$")))
+  (defun flymake-create-temp-intemp (file-name prefix)
+    "Return file name in temporary directory for checking FILE-NAME.
+This is a replacement for `flymake-create-temp-inplace'. The
+difference is that it gives a file name in
+`temporary-file-directory' instead of the same directory as
+FILE-NAME.
+
+For the use of PREFIX see that function.
+
+Note that not making the temporary file in another directory
+\(like here) will not if the file you are checking depends on
+relative paths to other files \(for the type of checks flymake
+makes)."
+    (unless (stringp file-name)
+      (error "Invalid file-name"))
+    (or prefix
+        (setq prefix "flymake"))
+    (let* ((name (concat
+                  (file-name-nondirectory
+                   (file-name-sans-extension file-name))
+                  "_" prefix))
+           (ext  (concat "." (file-name-extension file-name)))
+           (temp-name (make-temp-file name nil ext))
+           )
+      (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
+      temp-name))
   (defun flymake-pylint-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
+                       'flymake-create-temp-intemp))
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
