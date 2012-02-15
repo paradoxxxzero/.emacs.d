422e938a2d6ddc3bcf6c4a47c71ef0efc331be50
Florian Mounier <florian.mounier@kozea.fr>
2012-02-15 12:23:06 +0100

    Update flymake

--- .emacs.d/elisp/_init.el
+++ .emacs.d/elisp/_init.el
@@ -10,7 +10,6 @@
         ))
 
 ;; Autoloads
-(add-to-list 'load-path "~/.emacs.d/elisp/emacs-for-python/extensions/")
 (add-to-list 'load-path "~/.emacs.d/elisp/lua-mode/")
 (autoload 'lua-mode "lua-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
@@ -47,18 +46,25 @@
 (autoload 'sass-mode "sass-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
 
-
-
-;; (add-to-list 'load-path "~/.emacs.d/elisp/minimap/")
-;; (require 'minimap)
-
-;; (add-to-list 'load-path "~/.emacs.d/elisp/mingus/")
-;; (require 'mingus)
-
 (autoload 'cython-mode "cython-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.pyx\\'" . cython-mode))
 (add-to-list 'auto-mode-alist '("\\.pxd\\'" . cython-mode))
 (add-to-list 'auto-mode-alist '("\\.pxi\\'" . cython-mode))
+
+(add-to-list 'load-path "~/.emacs.d/elisp/flymake")
+(require 'flymake)
+(defun flymake-python-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-copy))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "~/.emacs.d/pycheckers" (list local-file))))
+(add-to-list 'flymake-allowed-file-name-masks '("\\.py\\'" flymake-python-init))
+(add-hook 'python-mode-hook 'flymake-find-file-hook)
+(add-to-list 'load-path "~/.emacs.d/elisp/flymake-cursor")
+(require 'flymake-cursor)
+
 
 (add-to-list 'load-path "~/.emacs.d/elisp/full-ack")
 (autoload 'ack-same "full-ack" nil t)
@@ -90,47 +96,26 @@
 
 (setq ido-enable-flex-matching t) ;; enable fuzzy matching
 
-;; Flymake
-(when (load-file "~/.emacs.d/elisp/emacs-for-python/extensions/flymake-patch.el")
-  (setq flymake-info-line-regex
-        (append flymake-info-line-regex '("unused$" "^redefinition" "used$")))
-  (defun flymake-create-temp-intemp (file-name prefix)
-    "Return file name in temporary directory for checking FILE-NAME.
-This is a replacement for `flymake-create-temp-inplace'. The
-difference is that it gives a file name in
-`temporary-file-directory' instead of the same directory as
-FILE-NAME.
+(add-to-list 'load-path "~/.emacs.d/elisp/pylookup")
+(setq pylookup-dir "~/.emacs.d/elisp/pylookup")
+(add-to-list 'load-path pylookup-dir)
 
-For the use of PREFIX see that function.
+;; load pylookup when compile time
+(eval-when-compile (require 'pylookup))
 
-Note that not making the temporary file in another directory
-\(like here) will not if the file you are checking depends on
-relative paths to other files \(for the type of checks flymake
-makes)."
-    (unless (stringp file-name)
-      (error "Invalid file-name"))
-    (or prefix
-        (setq prefix "flymake"))
-    (let* ((name (concat
-                  (file-name-nondirectory
-                   (file-name-sans-extension file-name))
-                  "_" prefix))
-           (ext  (concat "." (file-name-extension file-name)))
-           (temp-name (make-temp-file name nil ext))
-           )
-      (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
-      temp-name))
-  (defun flymake-pylint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-intemp)))
-      (list "~/.emacs.d/pycheckers" (list temp-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pylint-init))
-  (load-library "flymake-cursor"))
+;; set executable file and db file
+(setq pylookup-program (concat pylookup-dir "/pylookup.py"))
+(setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
 
-(eval-after-load 'python
-  (add-hook 'python-mode-hook 'flymake-find-file-hook))
+;; set search option if you want
+;; (setq pylookup-search-options '("--insensitive" "0" "--desc" "0"))
 
+;; to speedup, just load it on demand
+(autoload 'pylookup-lookup "pylookup"
+  "Lookup SEARCH-TERM in the Python HTML indexes." t)
+
+(autoload 'pylookup-update "pylookup" 
+  "Run pylookup-update and create the database at `pylookup-db-file'." t)
 
 (standard-display-ascii ?\t "↹    ")
 
@@ -188,5 +173,6 @@ makes)."
 (global-set-key (kbd "s-C-<down>") 'enlarge-window)
 
 (global-set-key (kbd "<XF86Calculator>") 'flymake-start-syntax-check)
+(global-set-key (kbd "<XF86Mail>") 'pylookup-lookup)
 
 (server-start)
